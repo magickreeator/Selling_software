47b20fd59caf0bd0b514f922096a992bafa07ac7
студент20 <20@college.ru>
2015-01-21 08:45:51 +0300

    добавил меню

--- Продажа программного обеспечения/categoryForm.Designer.cs
+++ Продажа программного обеспечения/categoryForm.Designer.cs
@@ -29,7 +29,15 @@
         private void InitializeComponent()
         {
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.добавитьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.редактироватьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.удалитьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.фильтрацияToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.отчетToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ещёОдинОтчётToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // dataGridView1
@@ -40,6 +48,57 @@
             this.dataGridView1.Size = new System.Drawing.Size(575, 213);
             this.dataGridView1.TabIndex = 0;
             // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.добавитьToolStripMenuItem,
+            this.редактироватьToolStripMenuItem,
+            this.удалитьToolStripMenuItem,
+            this.фильтрацияToolStripMenuItem,
+            this.отчетToolStripMenuItem,
+            this.ещёОдинОтчётToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(599, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // добавитьToolStripMenuItem
+            // 
+            this.добавитьToolStripMenuItem.Name = "добавитьToolStripMenuItem";
+            this.добавитьToolStripMenuItem.Size = new System.Drawing.Size(71, 20);
+            this.добавитьToolStripMenuItem.Text = "Добавить";
+            // 
+            // редактироватьToolStripMenuItem
+            // 
+            this.редактироватьToolStripMenuItem.Name = "редактироватьToolStripMenuItem";
+            this.редактироватьToolStripMenuItem.Size = new System.Drawing.Size(99, 20);
+            this.редактироватьToolStripMenuItem.Text = "Редактировать";
+            // 
+            // удалитьToolStripMenuItem
+            // 
+            this.удалитьToolStripMenuItem.Name = "удалитьToolStripMenuItem";
+            this.удалитьToolStripMenuItem.Size = new System.Drawing.Size(63, 20);
+            this.удалитьToolStripMenuItem.Text = "Удалить";
+            // 
+            // фильтрацияToolStripMenuItem
+            // 
+            this.фильтрацияToolStripMenuItem.Name = "фильтрацияToolStripMenuItem";
+            this.фильтрацияToolStripMenuItem.Size = new System.Drawing.Size(86, 20);
+            this.фильтрацияToolStripMenuItem.Text = "Фильтрация";
+            // 
+            // отчетToolStripMenuItem
+            // 
+            this.отчетToolStripMenuItem.Name = "отчетToolStripMenuItem";
+            this.отчетToolStripMenuItem.Size = new System.Drawing.Size(51, 20);
+            this.отчетToolStripMenuItem.Text = "Отчет";
+            // 
+            // ещёОдинОтчётToolStripMenuItem
+            // 
+            this.ещёОдинОтчётToolStripMenuItem.Name = "ещёОдинОтчётToolStripMenuItem";
+            this.ещёОдинОтчётToolStripMenuItem.Size = new System.Drawing.Size(105, 20);
+            this.ещёОдинОтчётToolStripMenuItem.Text = "Ещё один отчёт";
+            // 
             // categoryForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -47,17 +106,29 @@
             this.BackgroundImage = global::Продажа_программного_обеспечения.Properties.Resources.footer_lodyas;
             this.ClientSize = new System.Drawing.Size(599, 403);
             this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "categoryForm";
             this.Text = "Категории";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.categoryForm_FormClosed);
             this.Load += new System.EventHandler(this.categoryForm_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem добавитьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem редактироватьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem удалитьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem фильтрацияToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem отчетToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ещёОдинОтчётToolStripMenuItem;
     }
 }
