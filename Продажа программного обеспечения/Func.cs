6c2b27f292958ed66cf42c094e4f4604ee3b83cf
студент20 <20@college.ru>
2015-01-22 12:52:10 +0300

    добавил форму для отчёта

--- Продажа программного обеспечения/Func.cs
+++ Продажа программного обеспечения/Func.cs
@@ -42,5 +42,30 @@ namespace Продажа_программного_обеспечения
             }
             return Selected;
         }
+
+        //public void updateTableData(string TableName, string param1, string param2, string param3, string param4, string param5)
+        //{
+        //    try
+        //    {
+        //        Form15 frm15 = new Form15();
+        //        using (SqlConnection conn = new SqlConnection(connect))
+        //        {
+        //            conn.Open();
+        //            using (SqlCommand Update = new SqlCommand("UPDATE Postavchiki SET Naimenovanie=@Naimenovanie,Site=@Site, Adres=@Adres, Kod_postavki=@Kod_postavki WHERE Kod_postavshika=@Kod_postavshika", conn))
+        //            {
+        //                Update.Parameters.AddWithValue("@Naimenovanie", Naimenovanie);
+        //                Update.Parameters.AddWithValue("@Site", Site);
+        //                Update.Parameters.AddWithValue("@Kod_postavshika", Kod_postavshika);
+        //                Update.Parameters.AddWithValue("@Adres", Adres);
+        //                Update.Parameters.AddWithValue("@Kod_postavki", Kod_postavki);
+        //                Update.ExecuteNonQuery();
+        //            }
+        //        }
+        //    }
+        //    catch (SqlException ex)
+        //    {
+        //        MessageBox.Show(ex.ToString());
+        //    }
+        //}
     }
 }
---
+++ Продажа программного обеспечения/reportForm.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace Продажа_программного_обеспечения
+{
+    partial class reportForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.reportBrowser = new System.Windows.Forms.WebBrowser();
+            this.SuspendLayout();
+            // 
+            // reportBrowser
+            // 
+            this.reportBrowser.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.reportBrowser.Location = new System.Drawing.Point(0, 0);
+            this.reportBrowser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.reportBrowser.Name = "reportBrowser";
+            this.reportBrowser.Size = new System.Drawing.Size(787, 612);
+            this.reportBrowser.TabIndex = 0;
+            // 
+            // reportForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(787, 612);
+            this.Controls.Add(this.reportBrowser);
+            this.Name = "reportForm";
+            this.Text = "Отчёт";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.reportForm_FormClosed);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser reportBrowser;
+    }
+}
